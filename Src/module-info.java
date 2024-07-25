e137ccc9a19e906b0a2e6fa45c18d24fc51d23d9
Thanh Tùng <electricthanhtung@gmail.com>
2024-07-25 23:54:12 +0700

    Base JDK for FlintJVM

---
+++ Src/module-info.java
@@ -0,0 +1,9 @@
+
+module java.base {
+    exports java.io;
+    exports java.lang;
+    exports java.math;
+    exports java.util;
+    exports jdk.internal.math;
+    exports java.lang.annotation;
+}
