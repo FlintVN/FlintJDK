2baba2853b54bf0148920166355d3cff0159b06e
Thanh Tùng <electricthanhtung@gmail.com>
2024-10-30 12:43:09 +0700

    Add java.util.function.Consumer interface

---
+++ Src/java.base/java/util/function/Consumer.java
@@ -0,0 +1,18 @@
+package java.util.function;
+
+@FunctionalInterface
+public interface Consumer<T> {
+    void accept(T t);
+
+    default Consumer<T> andThen(Consumer<? super T> after) {
+        if(after == null)
+            throw new NullPointerException();
+        return new Consumer<T>() {
+            @Override
+            public void accept(T t) {
+                accept(t);
+                after.accept(t);
+            }
+        };
+    }
+}
