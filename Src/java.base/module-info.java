278a591e59df4994d06b665e6b5d408016b64f94
Thanh Tùng <electricthanhtung@gmail.com>
2024-12-09 18:28:17 +0700

    Add package java.util.function

--- Src/java.base/module-info.java
+++ Src/java.base/module-info.java
@@ -4,6 +4,7 @@ module java.base {
     exports java.lang;
     exports java.math;
     exports java.util;
+    exports java.util.function;
     exports jdk.internal.math;
     exports jdk.internal.vm.annotation;
     exports java.lang.annotation;
