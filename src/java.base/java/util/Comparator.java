1718863ce47fb1a282eb48daa74a7f5bd3d53cd8
Thanh Tùng <electricthanhtung@gmail.com>
2025-04-17 23:59:10 +0700

    Implement Comparator.reversed and Comparator.reverseOrder methods

--- src/java.base/java/util/Comparator.java
+++ src/java.base/java/util/Comparator.java
@@ -13,9 +13,7 @@ public interface Comparator<T> {
     boolean equals(Object obj);
 
     default Comparator<T> reversed() {
-        // TODO
-        // return Collections.reverseOrder(this);
-        throw new UnsupportedOperationException();
+        return Collections.reverseOrder(this);
     }
 
     default Comparator<T> thenComparing(Comparator<? super T> other) {
@@ -50,9 +48,7 @@ public interface Comparator<T> {
     }
 
     public static <T extends Comparable<? super T>> Comparator<T> reverseOrder() {
-        // TODO
-        // return Collections.reverseOrder();
-        throw new UnsupportedOperationException();
+        return Collections.reverseOrder();
     }
 
     @SuppressWarnings("unchecked")
