79e184ce3b675fbcbcc6be377e9984aade362f40
Thanh Tùng <electricthanhtung@gmail.com>
2025-11-16 15:01:59 +0700

    Add flint.io.SerialPort to support SerialPort (COM/UART)

---
+++ src/flint.io/flint/machine/Parity.java
@@ -0,0 +1,25 @@
+package flint.machine;
+
+public enum Parity {
+    NONE(0),
+    ODD(1),
+    EVEN(2),
+    MARK(3),
+    SPACE(4);
+
+    final int value;
+
+    private Parity(int value) {
+        this.value = value;
+    }
+
+    public static Parity fromValue(int value) {
+        return switch(value) {
+            case 0 -> NONE;
+            case 1 -> ODD;
+            case 2 -> EVEN;
+            case 3 -> MARK;
+            default -> SPACE;
+        };
+    }
+}
