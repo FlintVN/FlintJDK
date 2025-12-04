dec2230436dfdcc53edb1cb0640339a9369f92b8
Thanh Tùng <electricthanhtung@gmail.com>
2025-12-04 21:18:43 +0700

    Add support for flint.machine.Dac

---
+++ src/flint.io/flint/machine/Dac.java
@@ -0,0 +1,17 @@
+package flint.machine;
+
+import java.io.IOException;
+
+public class Dac {
+    private String name;
+    private int dacId = -1;
+
+    private native void initDac();
+
+    public Dac(String name) {
+        this.name = name;
+        initDac();
+    }
+
+    public native void write(int value);
+}
