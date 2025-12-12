e45db24c92917a47a4240a3644168206f6985cd0
Thanh Tùng <electricthanhtung@gmail.com>
2025-12-12 21:47:49 +0700

    Add support Character.forDigit method

--- src/java.base/java/lang/Character.java
+++ src/java.base/java/lang/Character.java
@@ -76,6 +76,16 @@ public final class Character implements Comparable<Character> {
         return String.valueOf(c);
     }
 
+    public static char forDigit(int digit, int radix) {
+        if((digit >= radix) || (digit < 0))
+            return '\0';
+        if((radix < Character.MIN_RADIX) || (radix > Character.MAX_RADIX))
+            return '\0';
+        if(digit < 10)
+            return (char)('0' + digit);
+        return (char)('a' - 10 + digit);
+    }
+
     @Override
     public int compareTo(Character anotherCharacter) {
         return value - anotherCharacter.value;
