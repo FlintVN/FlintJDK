7276eb26d2d51035c936db6191cd33956ea841c1
Thanh Tùng <electricthanhtung@gmail.com>
2025-12-13 13:57:43 +0700

    Support java.net.InetAddress

---
+++ src/java.base/java/net/UnknownHostException.java
@@ -0,0 +1,13 @@
+package java.net;
+
+import java.io.IOException;
+
+public class UnknownHostException extends IOException {
+    public UnknownHostException(String host) {
+        super(host);
+    }
+
+    public UnknownHostException() {
+
+    }
+}
