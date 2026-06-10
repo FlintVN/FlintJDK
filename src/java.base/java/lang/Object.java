9c68d75f2f4d2bc4d3f4671db2970e3ffaad3c33
Thanh Tùng <electricthanhtung@gmail.com>
2026-06-10 10:56:30 +0700

    Support Object.notify Object.notifyAll and Object.wait

--- src/java.base/java/lang/Object.java
+++ src/java.base/java/lang/Object.java
@@ -1,5 +1,7 @@
 package java.lang;
 
+import jdk.internal.vm.annotation.IntrinsicCandidate;
+
 public final class Object {
     public Object() {
 
@@ -17,5 +19,27 @@ public final class Object {
 
     public String toString() {
         return getClass().getName() + "@" + Integer.toHexString(hashCode());
+    }
+
+    @IntrinsicCandidate
+    public final native void notify();
+
+    @IntrinsicCandidate
+    public final native void notifyAll();
+
+    public final native void wait(long timeoutMillis) throws InterruptedException;
+
+    public final void wait() throws InterruptedException {
+        wait(0L);
+    }
+
+    public final void wait(long timeoutMillis, int nanos) throws InterruptedException {
+        if(timeoutMillis < 0)
+            throw new IllegalArgumentException("timeoutMillis value is negative");
+        if(nanos < 0 || nanos > 999999)
+            throw new IllegalArgumentException("nanosecond timeout value out of range");
+        if(nanos > 0 && timeoutMillis < Long.MAX_VALUE)
+            timeoutMillis++;
+        wait(timeoutMillis);
     }
 }
