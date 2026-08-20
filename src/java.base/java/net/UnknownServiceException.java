694c58343260035d5a0bc445c8629a5d32edd106
Thanh Tùng <electricthanhtung@gmail.com>
2026-08-20 10:45:08 +0700

    Add support for a few more exception classes

---
+++ src/java.base/java/net/UnknownServiceException.java
@@ -0,0 +1,13 @@
+package java.net;
+
+import java.io.IOException;
+
+public class UnknownServiceException extends IOException {
+    public UnknownServiceException() {
+
+    }
+
+    public UnknownServiceException(String msg) {
+        super(msg);
+    }
+}
